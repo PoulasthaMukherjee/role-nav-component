d5ded101083e4e4c4210ea8ff475cb8d71e164ea
Poulastha Mukherjee <poulasthamukherjee01@gmail.com>
2024-10-16 13:26:52 +0530

    Update Protected.jsx

--- src/components/Protected.jsx
+++ src/components/Protected.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
-import { Navigate } from 'react-router-dom';
+import { Navigate, useLocation } from 'react-router-dom';
 
 const Protected = ({ userRole, children }) => {
-  if (userRole === 'analyst' && window.location.pathname.startsWith('/projects')) {
+  const location = useLocation();
+  if (userRole === 'analyst' && location.pathname.startsWith('/projects')) {
     return <Navigate to="/" />;
   }
 
