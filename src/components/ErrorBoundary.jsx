ed49ebda636303cb6f8b7d24ea6ce42ac9c6014c
Poulastha Mukherjee <poulasthamukherjee01@gmail.com>
2024-10-16 13:26:20 +0530

    Update ErrorBoundary.jsx

--- src/components/ErrorBoundary.jsx
+++ src/components/ErrorBoundary.jsx
@@ -1,19 +1,21 @@
 import React from 'react';
 import { ErrorBoundary } from 'react-error-boundary';
+import { useNavigate } from 'react-router-dom';
 
 const ErrorFallback = ({ error, resetErrorBoundary }) => (
   <div role="alert">
     <p>Something went wrong:</p>
     <pre>{error.message}</pre>
-    <button onClick={resetErrorBoundary}>Try again</button>
+    <button onClick={resetErrorBoundary}>Home</button>
   </div>
 );
 
 const CustomErrorBoundary = ({ children }) => {
+  const navigate = useNavigate();
   return (
     <ErrorBoundary
       FallbackComponent={ErrorFallback}
-      onReset={() => window.location.reload()}
+      onReset={() => navigate('/')}
     >
       {children}
     </ErrorBoundary>
