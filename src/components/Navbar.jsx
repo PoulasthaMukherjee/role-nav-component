75882d16e0ba6dc0a4a302917ff5f8e0d50088a8
Poulastha Mukherjee <poulasthamukherjee01@gmail.com>
2024-10-18 13:54:45 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { logoutUser } from '../redux/userSlice';
 
 const Navbar = () => {
+  const dispatch = useDispatch();
   const currentUser = useSelector((state) => state.user.currentUser);
-
   if (!currentUser) return null;
+
+  const handleLogout = () => {
+    dispatch(logoutUser());
+  };
 
   return (
     <nav>
@@ -28,6 +33,9 @@ const Navbar = () => {
             <Link to="/jobs">Jobs</Link>
           </li>
         )}
+        <li>
+          <Link onClick={handleLogout}>Logout</Link>
+        </li>
       </ul>
     </nav>
   );
